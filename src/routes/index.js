303c532585d667482bc76ce9290f563fad413fd9
dudoan <dudoanjr10@gmail.com>
2022-06-22 17:10:10 +0700

    Dung khung layout default

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import { HeaderOnly } from '~/components/Layout';
+
+import Home from '~/pages/Home';
+import Search from '~/pages/Search';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/profile', component: Profile },
+    { path: '/following', component: Following },
+    { path: '/search', component: Search, layout: null },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
