4893c5e3cb08f28e52040c78e609e74839822a79
dudoan <dudoanjr10@gmail.com>
2022-07-10 23:55:04 +0700

    Lesson 115

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,11 +6,13 @@ import Search from '~/pages/Search';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
+import Live from '~/pages/Live';
 
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Profile },
     { path: config.routes.profile, component: Following },
+    { path: config.routes.live, component: Live },
     { path: config.routes.upload, component: Search, layout: null },
     { path: config.routes.search, component: Upload, layout: HeaderOnly },
 ];
